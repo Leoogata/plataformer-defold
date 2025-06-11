a1ecf7aa22ec3b3da22b1b6c56bef76ee1504432
Leoogata <leonardo.pedrosa@sou.inteli.edu.br>
2025-06-11 04:34:39 -0300

    feat: adiciona cenário e personagem teste

---
+++ main/player.go
@@ -0,0 +1,15 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/player.tilesource\"\n"
+  "}\n"
+  ""
+  position {
+    x: 69.0
+    y: 72.0
+  }
+}
